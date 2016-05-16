dba8a0cc1027ca073f00acdb0acdbac36b6b7e8c
ashleyhu <ashleyhu97@gmail.com>
2016-05-16 18:28:40 -0400

    Old Website

---
+++ assets/php/ajax-load-more/ajax-load-more.php
@@ -0,0 +1,547 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	
+	<title>Ashley Hu - Graphic Designer, Prospective System Design Engineer</title>
+	
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="assets/images/favicon.png">
+	<link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/images/apple-touch-icon-114x114.png">
+	
+	<!-- Bootstrap core CSS -->
+	<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- Plugins -->
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+	<link href="assets/css/ionicons.min.css" rel="stylesheet">
+	<link href="assets/css/simpletextrotator.css" rel="stylesheet">
+	<link href="assets/css/magnific-popup.css" rel="stylesheet">
+	<link href="assets/css/owl.carousel.css" rel="stylesheet">
+	<link href="assets/css/superslides.css" rel="stylesheet">
+	<link href="assets/css/vertical.css" rel="stylesheet">
+	<link href="assets/css/animate.css" rel="stylesheet">
+	
+	<!-- Template core CSS -->
+	<link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+
+	<!-- PRELOADER -->
+	<div class="page-loader">
+		<div class="loader" style="color:black;">A few more lines of code...</div>
+	</div>
+	<!-- /PRELOADER -->
+
+	<!-- WRAPPER -->
+	<div class="wrapper">
+
+		<!-- NAVIGATION -->
+		<nav class="navbar navbar-custom navbar-transparent navbar-light navbar-fixed-top">
+
+			<div class="container">
+			
+				<div class="navbar-header">
+					<!-- YOU LOGO HERE -->
+					<a class="navbar-brand" href="#hero">
+						<!-- IMAGE OR SIMPLE TEXT -->
+						<img src="assets/images/profile.jpg" alt="profile" class="clearfix img-responsive img-circle" style="margin-top:-10px; height:175%; display:inline-block;">
+						<h2 style="margin-top:-15px; display:inline-block;">Ashley Hu</h1>
+					</a>
+				</div>
+			
+				<!-- /ICONS NAVBAR -->
+			
+				<ul class="extra-navbar nav navbar-nav navbar-right">
+					<li><a href="#hero" title="Home">Home</a></li>
+					<li><a href="#portfolio" title="Portfolio">Portfolio</a></li>
+					<li><a href="assets/Resume_Design.pdf" title="Contact">Resume</a></li>
+				</ul>
+			
+			</div>
+
+		</nav>
+		<!-- /NAVIGATION -->
+
+		<!-- HERO -->
+		<section id="hero" class="module-hero module-parallax module-full-height bg-light-90" data-background="assets/images/section-3.jpg">
+
+			<!-- HERO TEXT -->
+			<div class="hero-caption">
+				<div class="hero-text">
+
+					<h1 class="mh-line-size-1 font-alt m-b-50">Ashley Hu</h1>
+					<h5 class="mh-line-size-2 font-alt"><span class="rotate"><b>Innovative Designer | UWaterloo Engineering Student</b></span></h5>
+
+				</div>
+			</div>
+			<!-- /HERO TEXT -->
+
+		</section>
+		<!-- /HERO -->
+
+		<!-- PORTFOLIO -->
+		<section id="portfolio" class="module">
+
+			<div class="container">
+				<div class="row text-center">
+					<h1 class="mh-line-size-1 font-alt">Portfolio</h1><br>
+				</div>
+
+				<!-- FILTER -->
+				<div class="row">
+
+					<div class="col-sm-12">
+						<ul id="filters" class="filters font-alt">
+							<li><a href="#" data-filter="*" class="current">ALL <sup><small>.26</small></sup></a></li>
+							<li><a href="#" data-filter=".autocad">AUTOCAD <sup><small>.4</small></sup></a></li>
+							<li><a href="#" data-filter=".design">DESIGN <sup><small>.10</small></sup></a></li>
+							<li><a href="#" data-filter=".creative">CREATIVE <sup><small>.12</small></sup></a></li>
+							<li><a href="#" data-filter=".wood">WOODWORK <sup><small>.1</small></sup></a></li>
+						</ul>
+					</div>
+
+				</div>
+				<!-- /FILTER -->
+
+				<!-- WORKS GRID -->
+				<div class="row">
+
+					<div id="works-grid" class="works-grid works-hover-w">
+
+						<!-- DO NOT DELETE THIS DIV -->
+						<div class="grid-sizer"></div>
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item wide design">
+							<a href="assets/images/portfolio/KisKare Banner copy-min.jpg" class="gallery" title="KisKare Banner">
+								<img src="assets/images/portfolio/KisKare Banner copy-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">KisKare Banner</h3>
+									<div class="work-descr">
+										Design
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item wide creative">
+							<a href="assets/images/portfolio/Chicken Attack-min.jpg" class="gallery" title="Creative Artwork -  Chicken Attack">
+								<img src="assets/images/portfolio/Chicken Attack-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Creative Artwork -  Chicken Attack</h3>
+									<div class="work-descr">
+										Creative
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item wide-tall autocad">
+							<a href="assets/images/portfolio/X.png" class="gallery" title="Speak Design">
+								<img src="assets/images/portfolio/Xa.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Speaker Design</h3>
+									<div class="work-descr">
+										AutoCAD
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item design logo">
+							<a href="assets/images/portfolio/Medina Language Center Logo-min.jpg" class="gallery" title="Medina Language Center Logo">
+								<img src="assets/images/portfolio/Medina Language Center Logo-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Medina Language Center Logo</h3>
+									<div class="work-descr">
+										Design | Logo
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item tall design logo">
+							<a href="assets/images/portfolio/KisKare Icon copy-min.jpg" class="gallery" title="KisKare Icon">
+								<img src="assets/images/portfolio/KisKare Icon copy-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">KisKare Icon</h3>
+									<div class="work-descr">
+										Design | Logo
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item design logo">
+							<a href="assets/images/portfolio/Airternative Logo-min.jpg" class="gallery" title="Airternative Logo">
+								<img src="assets/images/portfolio/Airternative Logo-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Airternative Logo</h3>
+									<div class="work-descr">
+										Design | Logo
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item wide design logo">
+							<a href="assets/images/portfolio/LumiVent Logo-min.jpg" class="gallery" title="LumiVent Logo">
+								<img src="assets/images/portfolio/LumiVent Logo-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">LumiVent Logo</h3>
+									<div class="work-descr">
+										Design | Logo
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item autocad">
+							<a href="assets/images/portfolio/A-min.jpg" class="gallery" title="Roller Coaster Design Part A">
+								<img src="assets/images/portfolio/A-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Roller Coaster Design A</h3>
+									<div class="work-descr">
+										AutoCAD
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item autocad">
+							<a href="assets/images/portfolio/B-min.jpg" class="gallery" title="Roller Coaster Design Part B">
+								<img src="assets/images/portfolio/B-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Roller Coaster Design B</h3>
+									<div class="work-descr">
+										Autocad
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item design">
+							<a href="assets/images/portfolio/1 copy-min.jpg" class="gallery" title="KisKare Product Design">
+								<img src="assets/images/portfolio/1 copy-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">KisKare Product Design</h3>
+									<div class="work-descr">
+										Design
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item wide design">
+							<a href="assets/images/portfolio/Principals & Advisor.png" class="gallery" title="Yearbook Design">
+								<img src="assets/images/portfolio/Principals & Advisora.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Yearbook Design</h3>
+									<div class="work-descr">
+										Design
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item design">
+							<a href="assets/images/portfolio/Medina Language Center Signage copy-min.jpg" class="gallery" title="Medina Language Center Signage">
+								<img src="assets/images/portfolio/Medina Language Center Signage copy-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Medina Language Center Signage</h3>
+									<div class="work-descr">
+										Design
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item creative">
+							<a href="assets/images/portfolio/Chrismas.png" class="gallery" title="Christmas Card Design">
+								<img class="img-responsive" src="assets/images/portfolio/Chrismasa.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Christmas Card Design</h3>
+									<div class="work-descr">
+										Creative
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item creative wood">
+							<a href="assets/images/portfolio/The Box - Side-min.jpg" class="gallery" title="Woodwork project - The box">
+								<img src="assets/images/portfolio/The Box - Side-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Woodwork project - The box</h3>
+									<div class="work-descr">
+										Creative | Woodwork
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item tall wide creative">
+							<a href="assets/images/portfolio/No Tomorrow-min.jpg" class="gallery" title="Creative Artwork A">
+								<img src="assets/images/portfolio/No Tomorrow-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Creative Artwork - No tomorrow</h3>
+									<div class="work-descr">
+										Creative
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item design">
+							<a href="assets/images/portfolio/1 3-min.jpg" class="gallery" title="Medina Language Center Brochure Inside">
+								<img src="assets/images/portfolio/1 3-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Medina Language Center Brochure Inside</h3>
+									<div class="work-descr">
+										Design
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item creative">
+							<a href="assets/images/portfolio/IMG_9664-min.jpg" class="gallery" title="Creative Artwork - Bae">
+								<img src="assets/images/portfolio/IMG_9664-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Creative Artwork - Bae</h3>
+									<div class="work-descr">
+										Creative
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item wide tall creative">
+							<a href="assets/images/portfolio/Cry Me An Apple-min.jpg" class="gallery" title="Creative Artwork - Cry me an apple">
+								<img src="assets/images/portfolio/Cry Me An Apple-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Creative Artwork - Cry me an apple</h3>
+									<div class="work-descr">
+										Creative
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item autocad">
+							<a href="assets/images/portfolio/C-min.jpg" class="gallery" title="Roller Coaster Design Part C">
+								<img src="assets/images/portfolio/C-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Roller Coaster Design C</h3>
+									<div class="work-descr">
+										AutoCAD
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item tall creative">
+							<a href="assets/images/portfolio/Little Birdy-min.jpg" class="gallery" title="Creative Artwork - Little Birdy">
+								<img src="assets/images/portfolio/Little Birdy-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Creative Artwork - Little Birdy</h3>
+									<div class="work-descr">
+										Creative
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item creative">
+							<a href="assets/images/portfolio/Always A Turtle-min.jpg" class="gallery" title="Creative Artwork - Always A Turtle">
+								<img src="assets/images/portfolio/Always A Turtle-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Creative Artwork - Always A Turtle</h3>
+									<div class="work-descr">
+										Creative
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item creative">
+							<a href="assets/images/portfolio/Look At Me-min.jpg" class="gallery" title="Creative Artwork - Look at me">
+								<img src="assets/images/portfolio/Look At Me-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Creative Artwork - Look at me</h3>
+									<div class="work-descr">
+										Creative
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item design">
+							<a href="assets/images/portfolio/ReplayIt Contest-min.jpg" class="gallery" title="ReplayIt Contest Poster">
+								<img src="assets/images/portfolio/ReplayIt Contest-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">ReplayIt Contest Poster</h3>
+									<div class="work-descr">
+										Design
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item wide tall creative">
+							<a href="assets/images/portfolio/One Lonely Tree-min.jpg" class="gallery" title="Creative Artwork - One Lonely Tree">
+								<img src="assets/images/portfolio/One Lonely Tree-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Creative Artwork - One Lonely Tree</h3>
+									<div class="work-descr">
+										Creative
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item creative">
+							<a href="assets/images/portfolio/Mr. Chicky-min.jpg" class="gallery" title="Creative Artwork - Mr Chicky">
+								<img src="assets/images/portfolio/Mr. Chicky-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Creative Artwork - Mr Chicky</h3>
+									<div class="work-descr">
+										Creative
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+						<!-- PORTFOLIO ITEM -->
+						<div class="work-item creative">
+							<a href="assets/images/portfolio/Once Upon A Time-min.jpg" class="gallery" title="Creative Artwork - Once Upon A Time">
+								<img src="assets/images/portfolio/Once Upon A Time-mina.jpg" alt="">
+								<div class="work-caption font-alt">
+									<h3 class="work-title">Creative Artwork - Once Upon A Time</h3>
+									<div class="work-descr">
+										Creative
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- /PORTFOLIO ITEM -->
+
+					</div>
+
+				</div>
+				<!-- /WORKS GRID -->
+
+
+			</div>
+
+		</section>
+		<!-- /PORTFOLIO -->
+
+		<!-- FOOTER -->
+		<footer class="module bg-light">
+
+			<div class="container">
+			
+				<div class="row">
+			
+					<div class="col-sm-12">
+			
+						<p class="copyright text-center m-b-0">© 2015 <a href="#">Ashley</a>, All Rights Reserved.</p>
+			
+					</div>
+			
+				</div>
+			
+			</div>
+
+		</footer>
+		<!-- /FOOTER -->
+
+	</div>
+	<!-- /WRAPPER -->
+
+	<!-- SCROLLTOP -->
+	<div class="scroll-up">
+		<a href="#totop"><i class="fa fa-angle-double-up"></i></a>
+	</div>
+
+	<!-- Javascript files -->
+	<script src="assets/js/jquery-2.1.3.min.js"></script>
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery.superslides.min.js"></script>
+	<script src="assets/js/jquery.mb.YTPlayer.min.js"></script>
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<script src="assets/js/jquery.simple-text-rotator.min.js"></script>
+	<script src="assets/js/imagesloaded.pkgd.js"></script>
+	<script src="assets/js/isotope.pkgd.min.js"></script>
+	<script src="assets/js/packery-mode.pkgd.min.js"></script>
+	<script src="assets/js/appear.js"></script>
+	<script src="assets/js/jquery.easing.1.3.js"></script>
+	<script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/jqBootstrapValidation.js"></script>
+	<script src="assets/js/jquery.fitvids.js"></script>
+	<script src="assets/js/jquery.parallax-1.1.3.js"></script>
+	<script src="assets/js/smoothscroll.js"></script>
+	<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	<script src="assets/js/gmaps.js"></script>
+	<script src="assets/js/contact.js"></script>
+	<script src="assets/js/custom.js"></script>
+
+</body>
+</html>
